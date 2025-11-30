fd9b040bbcb11c0e83d069d9c056632c80655b8a
multimokia <misty@multimokia.studio>
2025-11-30 13:53:13 -0500

    chore(packages): Upgrade to tailwind 4

--- src/components/Separator.vue
+++ src/components/Separator.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex flex-row w-3/4 place-items-center">
-    <hr class="flex-grow border-zinc-500"/>
-    <spinning-logo class="!w-1/12 p-3"/>
-    <hr class="flex-grow border-zinc-500"/>
+    <hr class="grow border-zinc-500"/>
+    <spinning-logo class="w-1/12! p-3"/>
+    <hr class="grow border-zinc-500"/>
   </div>
 </template>
 
